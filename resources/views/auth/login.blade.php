cc92d4ec1582a9a179f16f7b3f2a49e96d9a2dbd
Flavio Schoute <flavio03@outlook.com>
2021-12-07 14:44:41 +0100

    Added login feature, header, password strength
    Changed language

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,10 +1,11 @@
 <x-guest-layout>
+    <x-header href="{{ route('register') }}">Registeren</x-header>
+
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
         </x-slot>
 
-        <x-jet-validation-errors class="mb-4" />
+        <x-jet-validation-errors class="mb-4"/>
 
         @if (session('status'))
             <div class="mb-4 font-medium text-sm text-green-600">
@@ -21,21 +22,15 @@
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
+                <x-jet-label for="password" value="{{ __('Wachtwoord') }}" />
                 <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
             </div>
 
-            <div class="block mt-4">
+            <div class="flex items-center justify-between mt-4">
                 <label for="remember_me" class="flex items-center">
                     <x-jet-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    <span class="ml-2 text-sm text-gray-600">{{ __('Mij onthouden') }}</span>
                 </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
-                    {{ __('Nog niet geregisteerd?') }}
-                </a>
 
                 <x-jet-button class="ml-4">
                     {{ __('Inloggen') }}
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,13 @@
+<nav class="flex justify-between items-center h-16 shadow-lg font-semibold text-xl text-gray-800 leading-tight">
+    <div class="logo">
+        <h1 class="mr-8 cursor-pointer">Logo</h1>
+    </div>
+    <ul>
+        <li>
+            <a {{ $attributes }} class="mr-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
+                {{ $slot }}
+            </a>
+        </li>
+    </ul>
+</nav>
+
