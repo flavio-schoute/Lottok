faf6d3f7da66305d8c339c87673a1d2bcef09438
HornyProgrammer <80827915+HornyProgrammer@users.noreply.github.com>
2021-12-10 11:42:10 +0100

    cursor update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,70 +1,77 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="overflow-hidden">
+                <h2 class="font-semibold md:text-5xl text-4xl text-indigo-800 leading-tight mb-10 sm:m-0 sm:mb-10 ml-5 mr-5">
+                    {{ __('Wedstrijden') }}
+                </h2>
                 <div class="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-8 sm:m-0 ml-5 mr-5">
-                    <div class="bg-white h-60 flex flex-col justify-center items-center text-indigo-800 border-4 border-indigo-800 hover:bg-indigo-800 hover:text-white transition ease-in-out">
+                    <div class="bg-white h-60 flex flex-col justify-center items-center text-indigo-800 border-2 border-indigo-800 hover:bg-indigo-800 hover:text-white transition ease-in-out cursor-pointer">
     	                <h2 class="text-5xl font-medium">Uitslag</h2>
                         <br>
                         <h3 class="text-3xl font-bold">0 - 1</h3>
                         <br>
                         <h4 class="text-2xl font-semibold">AJAX VS PSV</h4>
                     </div>
-                    <div class="bg-white h-60 flex flex-col justify-center items-center text-indigo-800 border-4 border-indigo-800 hover:bg-indigo-800 hover:text-white transition ease-in-out">
+                    <div class="bg-white h-60 flex flex-col justify-center items-center text-indigo-800 border-2 border-indigo-800 hover:bg-indigo-800 hover:text-white transition ease-in-out cursor-pointer">
     	                <h2 class="text-5xl font-medium">Uitslag</h2>
                         <br>
                         <h3 class="text-3xl font-bold">0 - 1</h3>
                         <br>
                         <h4 class="text-2xl font-semibold">AJAX VS PSV</h4>
                     </div>
-                    <div class="bg-white h-60 flex flex-col justify-center items-center text-indigo-800 border-4 border-indigo-800 hover:bg-indigo-800 hover:text-white transition ease-in-out">
+                    <div class="bg-white h-60 flex flex-col justify-center items-center text-indigo-800 border-2 border-indigo-800 hover:bg-indigo-800 hover:text-white transition ease-in-out cursor-pointer">
     	                <h2 class="text-5xl font-medium">Uitslag</h2>
                         <br>
                         <h3 class="text-3xl font-bold">0 - 1</h3>
                         <br>
                         <h4 class="text-2xl font-semibold">AJAX VS PSV</h4>
                     </div>
-                    <div class="bg-white h-60 flex flex-col justify-center items-center text-indigo-800 border-4 border-indigo-800 hover:bg-indigo-800 hover:text-white transition ease-in-out">
+                    <div class="bg-white h-60 flex flex-col justify-center items-center text-indigo-800 border-2 border-indigo-800 hover:bg-indigo-800 hover:text-white transition ease-in-out cursor-pointer">
     	                <h2 class="text-5xl font-medium">Uitslag</h2>
                         <br>
                         <h3 class="text-3xl font-bold">0 - 1</h3>
                         <br>
                         <h4 class="text-2xl font-semibold">AJAX VS PSV</h4>
                     </div>
-                    <div class="bg-white h-60 flex flex-col justify-center items-center text-indigo-800 border-4 border-indigo-800 hover:bg-indigo-800 hover:text-white transition ease-in-out">
+                    <div class="bg-white h-60 flex flex-col justify-center items-center text-indigo-800 border-2 border-indigo-800 hover:bg-indigo-800 hover:text-white transition ease-in-out cursor-pointer">
     	                <h2 class="text-5xl font-medium">Uitslag</h2>
                         <br>
                         <h3 class="text-3xl font-bold">0 - 1</h3>
                         <br>
                         <h4 class="text-2xl font-semibold">AJAX VS PSV</h4>
                     </div>
-                    <div class="bg-white h-60 flex flex-col justify-center items-center text-indigo-800 border-4 border-indigo-800 hover:bg-indigo-800 hover:text-white transition ease-in-out">
+                    <div class="bg-white h-60 flex flex-col justify-center items-center text-indigo-800 border-2 border-indigo-800 hover:bg-indigo-800 hover:text-white transition ease-in-out cursor-pointer">
     	                <h2 class="text-5xl font-medium">Uitslag</h2>
                         <br>
                         <h3 class="text-3xl font-bold">0 - 1</h3>
                         <br>
                         <h4 class="text-2xl font-semibold">AJAX VS PSV</h4>
                     </div>
-                    <div class="bg-white h-60 flex flex-col justify-center items-center text-indigo-800 border-4 border-indigo-800 hover:bg-indigo-800 hover:text-white transition ease-in-out">
+                    <div class="bg-white h-60 flex flex-col justify-center items-center text-indigo-800 border-2 border-indigo-800 hover:bg-indigo-800 hover:text-white transition ease-in-out cursor-pointer">
     	                <h2 class="text-5xl font-medium">Uitslag</h2>
                         <br>
                         <h3 class="text-3xl font-bold">0 - 1</h3>
                         <br>
                         <h4 class="text-2xl font-semibold">AJAX VS PSV</h4>
                     </div>
-                    <div class="bg-white h-60 flex flex-col justify-center items-center text-indigo-800 border-4 border-indigo-800 hover:bg-indigo-800 hover:text-white transition ease-in-out">
+                    <div class="bg-white h-60 flex flex-col justify-center items-center text-indigo-800 border-2 border-indigo-800 hover:bg-indigo-800 hover:text-white transition ease-in-out cursor-pointer">
     	                <h2 class="text-5xl font-medium">Uitslag</h2>
                         <br>
                         <h3 class="text-3xl font-bold">0 - 1</h3>
                         <br>
                         <h4 class="text-2xl font-semibold">AJAX VS PSV</h4>
                     </div>
+                    <div class="bg-white h-60 flex flex-col justify-center items-center text-indigo-800 border-2 border-indigo-800 hover:bg-indigo-800 hover:text-white transition ease-in-out cursor-pointer">
+    	                <h2 class="text-5xl font-medium">Uitslag</h2>
+                        <br>
+                        <h3 class="text-3xl font-bold">0 - 1</h3>
+                        <br>
+                        <h4 class="text-2xl font-semibold">AJAX VS PSV</h4>
+                    </div>
+                    
                 </div>
             </div>
         </div>
