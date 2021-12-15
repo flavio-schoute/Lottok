0a7a0afff8d4ae13ad65157c09a9ba674150f090
Flavio Schoute <flavio03@outlook.com>
2021-12-15 13:38:30 +0100

    Added border and box shadow on gamle page

--- resources/views/game/index.blade.php
+++ resources/views/game/index.blade.php
@@ -5,19 +5,21 @@
                 <h2 class="font-semibold md:text-5xl text-4xl text-indigo-800 leading-tight mb-10 sm:m-0 sm:mb-10 ml-5 mr-5">
                     {{ __('Wedstrijd') }}
                 </h2>
-                <div class="container bg-white p-5 pt-12">
-                    <div class="title">
-                    @foreach($games as $game)
-                          <h2 class="text-4xl font-bold text-center">{{ $game->name1 }} VS {{ $game->name2 }}</h2>
-                       @endforeach
+                <div class="container bg-white p-5 pt-12 shadow-md border-2">
 
+                    <div class="title">
+                        @foreach($games as $game)
+                            <h2 class="text-4xl font-bold text-center">{{ $game->name1 }} VS {{ $game->name2 }}</h2>
+                        @endforeach
                     </div>
+
                     <div class="standing mt-12">
                     @foreach($games as $game)
                         <h2 class="text-3xl font-bold text-center">STAND</h2>
                         <h2 class="text-3xl font-medium text-center mt-5">4 - 1</h2>
                        @endforeach
                     </div>
+
                     <form class="mt-10 flex flex-col items-center justify-center">
                         <div class="team_and_multiplier">
                             <select class="text-center">
@@ -33,10 +35,10 @@
 
                         <div class="guess mt-10">
                             <input type="text" class="w-48 h-20 text-2xl">
-                        </div>  
+                        </div>
                         <button type="submit" class="mt-10 border border-solid border-black w-48 h-20 hover:bg-indigo-900 hover:text-white">Plaats gok</button>
                     </form>
-                    
+
 
                     <div class="goks_geplaatst">
 
